014b3a7b12919b7cff72e929d9601556a3cba048
Devin Castro <dcrocker21@gmail.com>
2020-08-23 21:51:36 -0700

    got started

---
+++ index.js
@@ -0,0 +1,4 @@
+const inquirer = require('inquirer')
+// Import the filsystem framework so we can use the writeFile function
+const fs = require('fs')
+// Import the module export from the generateMarkdown.js file
